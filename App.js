265d0dad872676d97fbc29c660a363e24a6a78a5
Harsh <harsh.patel@bacancytechnology.com>
2019-02-18 19:01:53 +0530

    getting image but not uploading

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, FlatList, Image } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, Image, TouchableOpacity } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
 
 // create a component
 class FetchAPIDemo extends Component {
@@ -8,23 +10,42 @@ class FetchAPIDemo extends Component {
     super(props);
     this.state = {
       isLoading: true,
-      dataSource: []
+      dataSource: [],
+      photo: null
     }
   }
 
-  componentDidMount() {
-    return fetch('https://pictshare.net/api/geturl.php?url=https://pictshare.net//8n8xib.jpg')
+  handleselect = () => {
+    console.log('photo url', this.state.photo.uri)
+    let data = new FormData();
+    const h = {};
+    h.Accept = 'application/json';
+    data.append('file', this.state.photo.uri)
+    return fetch('https://pictshare.net/api/upload.php', { method: 'POST', headers: h, body: data })
       .then((response) => response.json())
       .then((responseJson) => {
-        this.setState({ isLoading: false, dataSource: responseJson.url }, function () { });
+        this.setState({ isLoading: false, dataSource: responseJson.status }, function () { });
+        console.log('---', this.state.dataSource)
       })
       .catch((error) => {
         console.error(error);
       });
   }
 
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        this.setState({ photo: response });
+      }
+    });
+  };
+
   render() {
-    if (this.state.isLoading) {
+    const { isLoading, photo, dataSource } = this.state
+    if (false) {
       return (
         <View style={styles.container}>
           <ActivityIndicator />
@@ -33,20 +54,39 @@ class FetchAPIDemo extends Component {
     }
     return (
       <View>
-        <Text>data Loaded</Text>
+        <TouchableOpacity
+          onPress={this.handleChoosePhoto}
+        >
+          <Text style={{ fontSize: 20, marginHorizontal: 20 }}>select Image</Text>
+        </TouchableOpacity>
+        {photo && <TouchableOpacity
+          onPress={this.handleselect}
+        >
+          <Text style={{ fontSize: 20, marginHorizontal: 20 }}>upload</Text>
+        </TouchableOpacity>}
         {/* <FlatList
           data={this.state.dataSource}
           renderItem={({ item }) => <Text style={{ fontSize: 20 }}>{item}</Text>}
           keyExtractor={({ id }) => id}
         /> */}
-        <Text>{this.state.dataSource}</Text>
+        <Text>{dataSource}</Text>
         <View style={{ backgroundColor: 'red', height: 500, width: 400 }}>
           <Image
-            style={{ height: 100,width: 200 }}
-            source={{ uri: `${this.state.dataSource}` }}
+            style={{ height: 100, width: 200 }}
+            source={{ uri: `${dataSource}` }}
             resizeMode='cover'
           />
+          {photo &&
+            <Image
+              style={{ height: 100, width: 200, margin: 30 }}
+              source={{ uri: photo.uri }}
+              resizeMode='cover'
+            />
+          }
+
         </View>
+
+
       </View>
     )
   }
